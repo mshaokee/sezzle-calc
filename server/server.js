69921e141520e7ea55c98a9ed679fa254884050f
Shaokee Moua <m.shaokee@gmail.com>
2020-11-11 12:11:32 -0600

    button set up

--- server/server.js
+++ server/server.js
@@ -6,3 +6,25 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://sezzle-df3ce.firebaseio.com"
 });
+
+const db = admin.database();
+const ref = db.ref('history');
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+const server = app.listen(port);
+
+//WebSocket with socket.io
+
+//Static files from React
+app.use(express.static('public'));
+app.unsubscribe(bodyParser.json());
+
+//GET Route
+
+//POST Route
+
+//log listener
+console.log('Listening on port', port);
--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
 
 class Keypad extends Component {
     render() {
         return (
             <div>
-                <p>Keypad</p>
+                {/* clear button */}
+                <div>
+                    <Button variant="contained" className="button" name="CLEAR">CLEAR</Button>
+                </div>
+                {/* 7, 8, 9 + */}
+                
+                    <Button variant="contained" className="button" name="7">7</Button>
+                    <Button variant="contained" className="button" name="8">8</Button>
+                    <Button variant="contained" className="button" name="9">9</Button>
+                    <Button variant="contained" className="button" name="+">+</Button>
+                
+                {/* 4, 5, 6, * */}
+                <div>
+                    <Button variant="contained" className="4" name="4">4</Button>
+                    <Button variant="contained" className="5" name="5">5</Button>
+                    <Button variant="contained" className="6" name="6">6</Button>
+                    <Button variant="contained" className="*" name="*">*</Button>
+                </div>
+                {/* 1, 2, 3, / */}
+                <div>
+                    <Button variant="contained" className="1" name="1">1</Button>
+                    <Button variant="contained" className="2" name="2">2</Button>
+                    <Button variant="contained" className="3" name="3">3</Button>
+                    <Button variant="contained" className="/" name="/">/</Button>
+                </div>
+                {/* 0, ., -, = */}
+                <div>
+                    <Button variant="contained" className="0" name="0">0</Button>
+                    <Button variant="contained" className="." name=".">.</Button>
+                    <Button variant="contained" className="-" name="-">-</Button>
+                    <Button variant="contained" className="=" name="=">=</Button>
+                </div>
             </div>
         )
     }
